995e7e954079d9d8aaf7f36212979e38fab5db6b
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-23 15:54:44 +0530

    [Harman] ADD:program to demonstrate if round trip is possible from given starting point

---
+++ CircularTrip.java
@@ -0,0 +1,47 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class CircularTrip {
+    public static void main(String[] args) {
+        int[] petrol = {3, 2, 2};
+        int[] distance = {2,2,2};
+
+        int startingPoint = findStartingPoint(petrol, distance);
+        if (startingPoint != -1) {
+            System.out.println("Starting point: " + startingPoint);
+        } else {
+            System.out.println("No possible starting point");
+        }
+    }
+    public static int findStartingPoint(int[] petrol, int[] distance) {
+        int n = petrol.length;
+        int totalPetrol = 0;
+        int totalDistance = 0;
+        int startingPoint = 0;
+        int currentPetrol = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalPetrol += petrol[i];
+            totalDistance += distance[i];
+        }
+
+        if (totalPetrol < totalDistance) {
+            return -1; // No possible starting point
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            currentPetrol += petrol[i] - distance[i];
+            queue.add(currentPetrol);
+
+            if (currentPetrol < 0) {
+                // Reset queue and update starting point
+                queue.clear();
+                startingPoint = i + 1;
+                currentPetrol = 0;
+            }
+        }
+
+        return startingPoint;
+    }
+}
